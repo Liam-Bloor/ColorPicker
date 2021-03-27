3d404cd59c2e3c105944b13a5ceddbb826921267
Liam-Bloor <46817888+Liam-Bloor@users.noreply.github.com>
2021-03-27 15:58:20 +0000

    001-BasicLayout Added inputs and canvas necessary as well as styling to achieve a basic layout.

---
+++ src/components/HoverOverMenu.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export function HoverOverMenu() {
+	return (
+		<div className="hoverMenu">
+			<header className="hoverHeader">
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+			</header>
+		</div>
+	);
+}
